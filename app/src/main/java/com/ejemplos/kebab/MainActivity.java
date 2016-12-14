bc025946a902ab2dabd247775ffc717ec4613d62
Straiky <begainza@gmail.com>
2016-12-14 14:10:12 +0100

    Primera ventana "temporal"

--- app/src/main/java/com/ejemplos/kebab/MainActivity.java
+++ app/src/main/java/com/ejemplos/kebab/MainActivity.java
@@ -1,13 +1,34 @@
 package com.ejemplos.kebab;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+Button btnContacto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        btnContacto = (Button) findViewById(R.id.btnContacto);
         setContentView(R.layout.activity_main);
+        btnContacto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:0377778888"));
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
     }
 }
