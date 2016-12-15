d08904109f1176537b83af562b79246ef5d40c1b
kebabcebanc <ander5494@gmail.com>
2016-12-15 09:03:23 +0100

    Nuevas

--- app/src/main/java/com/ejemplos/kebab/Cliente.java
+++ app/src/main/java/com/ejemplos/kebab/Cliente.java
@@ -1,16 +1,42 @@
 package com.ejemplos.kebab;
 
+import android.app.Application;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by adminportatil on 14/12/2016.
  */
 
 public class Cliente extends AppCompatActivity{
+    EditText nom, ape, direc, tel, cp;
+    Button sig, salir, atras;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cliente);
+        nom=(EditText) findViewById(R.id.txtNombre);
+        ape=(EditText) findViewById(R.id.txtApellido);
+        direc=(EditText) findViewById(R.id.txtDireccion);
+        tel=(EditText) findViewById(R.id.txtTelefono);
+        cp=(EditText) findViewById(R.id.txtCP);
+        atras=(Button) findViewById(R.id.btnatras);
+        sig=(Button) findViewById(R.id.btnsiguiente);
+        salir=(Button) findViewById(R.id.btnsalir);
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/ejemplos/kebab/MainActivity.java
+++ app/src/main/java/com/ejemplos/kebab/MainActivity.java
@@ -11,13 +11,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-Button btnContacto;
-
+    Button btnContacto;
+    Button btnEmp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        btnContacto = (Button) findViewById(R.id.btnContacto);
         setContentView(R.layout.activity_main);
+        btnContacto = (Button) findViewById(R.id.btnContacto);
+        btnEmp = (Button) findViewById(R.id.btnEmpezar);
         btnContacto.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
@@ -30,5 +31,15 @@ Button btnContacto;
                 startActivity(callIntent);
             }
         });
+        btnEmp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lanzaAtras(null);
+            }
+        });
+    }
+    private void lanzaAtras(View view) {
+        Intent intent=new Intent(this, Cliente.class);
+        startActivity(intent);
     }
 }
